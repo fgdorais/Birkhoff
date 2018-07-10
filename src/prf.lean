050bc610768faf88dd12bee72f37c9f5ced93f31
François G. Dorais <fgdorais@gmail.com>
2018-07-10 12:27:42 -0400

    feat(prf): add prf_translate

--- src/prf.lean
+++ src/prf.lean
@@ -32,8 +32,8 @@ definition prf_symmetry {{t u : term sig}} : (ax ⊢ t ≡ u) → (ax ⊢ u ≡ 
 definition prf_transitivity {{t u v : term sig}} : (ax ⊢ t ≡ u) → (ax ⊢ u ≡ v) → (ax ⊢ t ≡ v) :=
 λ ptu puv, proof.euc t v u ptu (prf_symmetry puv)
 
-definition prf_substitution {sub : ℕ → term sig} : 
-Π {t u : term sig}, proof ax t u → proof ax (subst sub t) (subst sub u)
+definition prf_substitution (sub : ℕ → term sig) : 
+Π {{t u : term sig}}, proof ax t u → proof ax (subst sub t) (subst sub u)
 | ._ ._ (proof.axm ax i sub₀) :=
   have ht : subst (subst.comp sub₀ sub) (ax i).fst = subst sub (subst sub₀ (ax i).fst), by rw subst.subst_subst,
   have hu : subst (subst.comp sub₀ sub) (ax i).snd = subst sub (subst sub₀ (ax i).snd), by rw subst.subst_subst,
@@ -49,10 +49,20 @@ definition prf_substitution {sub : ℕ → term sig} :
   proof.euc (subst sub t) (subst sub u) (subst sub v) (prf_substitution htv) (prf_substitution huv)
 
 definition prf_replacement {{sub₁ sub₂ : ℕ → term sig}} (h : Π n, ax ⊢ sub₁ n ≡ sub₂ n) :
-Π t : term sig, ax ⊢ subst sub₁ t ≡ subst sub₂ t
+Π (t : term sig), ax ⊢ subst sub₁ t ≡ subst sub₂ t
 | (term.var n) := h n
 | (term.app s ts) :=
   have h₁ : term.app s (tup.map (subst sub₁) ts) = subst sub₁ (term.app s ts), by rw subst_app, 
   have h₂ : term.app s (tup.map (subst sub₂) ts) = subst sub₂ (term.app s ts), by rw subst_app,
   eq.rec_on h₁ (eq.rec_on h₂ (proof.app s (λ i, prf_replacement (ts i))))
 
+definition prf_translate {I' : Type*} {ax' : I' → eqn sig} (t : Π (i : I'), ax ⊢ (ax' i)) :
+∀ {{t u : term sig}}, proof ax' t u → proof ax t u
+| ._ ._ (proof.axm ax' i sub) := 
+  prf_substitution sub (t i)
+| ._ ._ (proof.var ax' n) := 
+  proof.var ax n
+| ._ ._ (proof.app s ps) := 
+  proof.app s (λ i, prf_translate (ps i))
+| ._ ._ (proof.euc t u v ptv puv) :=
+  proof.euc t u v (prf_translate ptv) (prf_translate puv)
