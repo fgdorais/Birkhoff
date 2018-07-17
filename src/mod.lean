6d975bb661ec60a173d450019ad4b1d91bb78f9f
François G. Dorais <fgdorais@gmail.com>
2018-07-17 11:41:43 -0400

    feat(mod): add namespace model

--- src/mod.lean
+++ src/mod.lean
@@ -14,18 +14,20 @@ definition models (e : eqn sig) : Prop :=
 
 infix ` ⊨ `:20 := models
 
-lemma mod_axiom (i : I) : ax ⊨ ax i := λ _ _ ha, ha i
+lemma model.axiom (i : I) : ax ⊨ ax i := λ _ _ ha, ha i
 
-lemma mod_reflexivity (t : term sig) : ax ⊨ t ≡ t :=
+namespace model
+
+lemma reflexivity (t : term sig) : ax ⊨ t ≡ t :=
 λ _ _ _ _, rfl
 
-lemma mod_symmetry {{t u : term sig}} : (ax ⊨ t ≡ u) → (ax ⊨ u ≡ t) :=
+lemma symmetry {{t u : term sig}} : (ax ⊨ t ≡ u) → (ax ⊨ u ≡ t) :=
 λ h _ a ha val, eq.symm (h a ha val)
 
-lemma mod_transitivity {{t u v : term sig}} : (ax ⊨ t ≡ u) → (ax ⊨ u ≡ v) → (ax ⊨ t ≡ v) :=
+lemma transitivity {{t u v : term sig}} : (ax ⊨ t ≡ u) → (ax ⊨ u ≡ v) → (ax ⊨ t ≡ v) :=
 λ htu huv α a ha val, eq.trans (htu a ha val) (huv a ha val)
 
-lemma mod_substitiution (sub : ℕ → term sig) {{t u : term sig}} :
+lemma substitiution (sub : ℕ → term sig) {{t u : term sig}} :
 (ax ⊨ t ≡ u) → (ax ⊨ subst sub t ≡ subst sub u) :=
 λ h _ a ha val, 
 calc
@@ -34,14 +36,14 @@ eval a val (subst sub t)
 ... = eval a (λ n, eval a val (sub n)) u  : by rw h a ha
 ... = eval a val (subst sub u)            : by rw subst_eval
 
-lemma mod_replacement {{sub₁ sub₂ : ℕ → term sig}} :
+lemma replacement {{sub₁ sub₂ : ℕ → term sig}} :
 (∀ n, ax ⊨ (sub₁ n) ≡ (sub₂ n)) → (∀ t, ax ⊨ subst sub₁ t ≡ subst sub₂ t)
 | h (term.var n) := 
   λ _ a ha val, h n a ha val
 | h (term.app s ts) :=
   λ _ a ha val,
   have tup.map (eval a val) (tup.map (subst sub₁) ts) = tup.map (eval a val) (tup.map (subst sub₂) ts),
-  from tup.ext (λ i, mod_replacement h (ts i) a ha val), 
+  from tup.ext (λ i, replacement h (ts i) a ha val), 
   calc 
   eval a val (subst sub₁ (term.app s ts))
       = eval a val (term.app s (tup.map (subst sub₁) ts)) : by rw subst_app
@@ -49,3 +51,5 @@ lemma mod_replacement {{sub₁ sub₂ : ℕ → term sig}} :
   ... = a.app s (tup.map (eval a val) (tup.map (subst sub₂) ts)) : by rw this
   ... = eval a val (term.app s (tup.map (subst sub₂) ts)) : by rw eval_app
   ... = eval a val (subst sub₂ (term.app s ts)) : by rw subst_app
+
+end model
